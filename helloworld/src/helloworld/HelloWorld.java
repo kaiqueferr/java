532e76469b55d705aef01fe00d63286b55b21dbc
Kaique <kaique.ferr@gmail.com>
2023-03-14 11:54:37 -0300

    1o commit - hello world

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello World!");
+		System.out.print("\nOla turma 62!");
+
+	}
+
+}
